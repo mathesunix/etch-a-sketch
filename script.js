6673b62fc0c72a6121470907c5a0f83af86908ba
Matheus <mag08081996@gmail.com>
2024-01-07 16:47:02 -0300

    Add button functionality, change canvas height
    The button functionality was broken before, now it's fixed. Added canvas height because it was missing before

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const canvas = document.querySelector('.container');
+const initialCanvasSize = 16 * 16;
+let currentCanvasSize = 0;
 const newCanvasButton = document.querySelector('.newCanvasButton');
 squaresArray = [];
 
-for (let i = 0; i < 16*16; i++) {
+for (let i = 0; i < initialCanvasSize; i++) {
     squaresArray[i] = document.createElement('div');
     squaresArray[i].style.cssText = 'width: 50px; height: 50px; flex-shrink: 0';
     squaresArray[i].addEventListener('mouseover', () => {
@@ -11,11 +13,28 @@ for (let i = 0; i < 16*16; i++) {
     canvas.appendChild(squaresArray[i]);
 }
 
+currentCanvasSize = 16 * 16;
+
 function newCanvas() {
-    /* for (let i = 0; i < 16*16; i++) {
-        squaresArray[i].style.backgroundColor = 'white';
-    } */
-    alert('Hello world!');
+    let newCanvasSize = prompt('Enter a new canvas size (max size is 100): ');
+
+    if (newCanvasSize > 100) {
+        newCanvasSize = 100;
+    }
+
+    for (let i = 0; i < currentCanvasSize; i++) {
+        canvas.removeChild(squaresArray[i]);
+    }
+    currentCanvasSize = newCanvasSize * newCanvasSize;
+
+    for (let i = 0; i < currentCanvasSize; i++) {
+        squaresArray[i] = document.createElement('div');
+        squaresArray[i].style.cssText = `width: ${800 / newCanvasSize}px; height: ${800 / newCanvasSize}px; flex-shrink: 0`; 
+        squaresArray[i].addEventListener('mouseover', () => {
+            squaresArray[i].style.backgroundColor = 'orange';
+        });
+        canvas.appendChild(squaresArray[i]);
+    }
 }
 
 newCanvasButton.addEventListener('click', newCanvas);
